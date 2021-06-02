7327c2c016bc32ad89c134cb1d09575b3d941cad
Lucy <lucyfox890@hotmail.com>
2021-06-02 16:47:35 +0100

    update

--- includes/processing_page.php
+++ includes/processing_page.php
@@ -6,11 +6,11 @@ $rating = intval($_POST['rating']);
 
 if (!isset($_POST['title']) && !is_string($_POST['title'])) {
     header("Location: functions.php?error=1");
-} if (!isset($_POST['year_of_release']) && !is_int($_POST['year_of_release'])) {
+} elseif (!isset($_POST['year_of_release']) && !is_int($_POST['year_of_release'])) {
     header("Location: functions.php?error=2");
-} if (isset($_POST['rating']) && is_int($_POST['rating'])){
+} if (isset($_POST['rating']) && is_int($_POST['rating'])) {
     header("Location: functions.php?error=3");
-} else {
+} elseif {
     $db = new PDO('mysql:host=db;dbname=top_movies', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_ASSOC);
     $query = $db->prepare("INSERT INTO films_list (title, year_of_release, rating) VALUES ('$title', '$year_of_release', '$rating')");
--- test/functions.php
+++ test/functions.php
@@ -10,8 +10,8 @@ class functions extends TestCase
     {
         $input = [
             ["title" => "It Happened One Night",
-            "year_of_release"=> "1934",
-            "rating"=> "99"]
+            "year_of_release" => "1934",
+            "rating" => "99"]
         ];
         $expected = "<h1>Top Rated Films</h1><br>Title: It Happened One Night<br>Year of Release: 1934<br>Rating: 99%<br><br>";
         $result = listMovies($input);
