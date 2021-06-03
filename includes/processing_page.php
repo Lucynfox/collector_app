3838bbc5dfbc773af756d68077cc10a8f1715d85
Lucy <lucyfox890@hotmail.com>
2021-06-03 09:54:00 +0100

    update

--- includes/processing_page.php
+++ includes/processing_page.php
@@ -1,13 +1,12 @@
 <?php
 
-//require '../index.php';
+require 'includes/functions.php';
 
 $title = $_POST['title'];
 $year_of_release = intval($_POST['year_of_release']);
 $rating = intval($_POST['rating']);
 
-$db = new PDO('mysql:host=db;dbname=top_movies', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_ASSOC);
+$db = connectToDB();
 $query = $db->prepare("INSERT INTO films_list (title, year_of_release, rating) VALUES ('$title', '$year_of_release', '$rating')");
 $query->execute();
 header("Location: http://localhost:1234/collector_app/index.php");
